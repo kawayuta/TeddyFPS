49c0f315ff4578f782ece2db6a40e49fd063ae1e
kawayuta <k216q013@stu.kyoto-seika.ac.jp>
2017-07-14 23:50:03 +0900

    initial

---
+++ Assets/script/TeddyRandom.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeddyRandom : MonoBehaviour {
+
+	public GameObject Teddyprefab;
+	float time = 3;
+	Vector3	endPosition;
+	private float startTime;
+	private Vector3 startPosition;
+
+	// Use this for initialization
+
+	void OnEnable ()
+	{
+		if (time <= 0) {
+			GameObject.Find ("Main Camera").transform.position = endPosition;
+			enabled = false;
+			return;
+		}
+
+		startTime = Time.timeSinceLevelLoad;
+		startPosition = GameObject.Find ("Main Camera").transform.position;
+	}
+
+	void Start () {
+
+		float ranXp = Random.Range (-200, 200);
+		float ranZp = Random.Range (-200, 200);
+		GameObject.Find ("meteddy").transform.position = new Vector3 (ranXp, 2.6f, ranZp);
+		for (int i = 0; i < 230; i++) {
+			
+			float ranX = Random.Range (-200, 200);
+			float ranY = Random.Range (30, 60);
+			float ranZ = Random.Range (-200, 200);
+			
+			Instantiate (Teddyprefab, new Vector3 (ranX, ranY, ranZ), Quaternion.identity);
+
+		}
+		StartCoroutine ("rote");
+
+	}
+
+	private IEnumerator rote() {
+		yield return new WaitForSeconds (2.5f);
+
+		for (float i = 90; i > 0; i--) {
+			GameObject.Find ("Main Camera").transform.rotation = Quaternion.Euler(i,0,0);
+			yield return null;  
+		}
+
+	}
+
+	void Update ()
+	{
+		Vector3 enda = GameObject.Find ("meteddy").transform.position;
+		var diff = Time.timeSinceLevelLoad - startTime;
+		if (diff > time) {
+			GameObject.Find ("Main Camera").transform.position = endPosition;
+			enabled = false;
+		}
+
+		var rate = diff / time;
+
+
+		GameObject.Find ("Main Camera").transform.position = Vector3.Lerp (startPosition, enda, rate);
+	}
+
+
+	void OnDrawGizmosSelected ()
+	{
+		#if UNITY_EDITOR
+
+		if( !UnityEditor.EditorApplication.isPlaying || enabled == false ){
+			startPosition = transform.position;
+		}
+
+		UnityEditor.Handles.Label(endPosition, endPosition.ToString());
+		UnityEditor.Handles.Label(startPosition, startPosition.ToString());
+		#endif
+		Gizmos.DrawSphere (endPosition, 0.1f);
+		Gizmos.DrawSphere (startPosition, 0.1f);
+
+		Gizmos.DrawLine (startPosition, endPosition);
+	}
+}
---
+++ Assets/script/me.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class me : MonoBehaviour {
+
+	CharacterController controller; 
+	Vector3 moveDirection; 
+	public GameObject Teddyprefab;
+	public bool check =true;
+	public bool get = false;
+	public int postCount;
+	public float speed = 1000;
+	float fSpeed = 53.0f; 
+	void Start () {
+		controller = GetComponent("CharacterController") as CharacterController; 
+	}
+	
+	void Update () {
+
+		Vector3 forward = GameObject.Find("Main Camera").transform.TransformDirection( Vector3.forward ); 
+		Vector3 right = GameObject.Find("Main Camera").transform.TransformDirection( Vector3.right ); 
+		moveDirection = Input.GetAxis("Horizontal") * right + Input.GetAxis("Vertical") * forward; 
+		moveDirection *= fSpeed; 
+
+		// 移動 
+		controller.Move( moveDirection * Time.deltaTime ); 
+
+
+		if (Input.GetKey ( KeyCode.F)) {
+			transform.Rotate (0, -1, 0);
+		}
+		if (Input.GetKey ( KeyCode.H)) {
+			transform.Rotate (0, 1, 0);
+		}			
+
+		raytest ();
+		if (Input.GetKey (KeyCode.B) && get == false && check == false) {
+
+			if (postCount > 0) {
+				
+				int Ca = postCount;
+				Vector3 nw = GameObject.Find ("meteddy").transform.position;
+				nw.z += 3;
+				Vector3 force;
+
+				GameObject bullets = GameObject.Instantiate (Teddyprefab, nw, Quaternion.identity)as GameObject;
+
+				force = this.gameObject.transform.forward * speed;
+				// Rigidbodyに力を加えて発射
+				bullets.GetComponent<Rigidbody>().AddForce (force);
+				postCount--;
+				if (Ca > postCount && postCount > 0) {
+					check = true;
+				}
+			}
+		} else {
+			check = false;
+		}
+	}
+
+	void raytest()
+	{
+		
+		Ray ray = new Ray (transform.position, new Vector3 (0, 0, 1));
+		RaycastHit hit;
+		int distance = 6;
+
+		Vector3 nw = GameObject.Find ("meteddy").transform.position;
+		nw.z += 3;
+		if (Physics.Raycast (ray, out hit, distance)) {
+			
+			if (Input.GetKey (KeyCode.Space) && get == false && check == false && hit.collider.tag == "teddy") {
+				Destroy (hit.collider.gameObject);
+				postCount++;
+				get = false;
+				check = false;
+			} else {
+				get = false;
+				check = true;
+			}
+
+		}
+	}
+		
+
+}
